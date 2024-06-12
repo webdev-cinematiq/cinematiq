745724948da96bf21594e2788de4fa6c20a9b19a
maikkatko <maikkatko@gmail.com>
2024-06-12 19:54:00 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import Home from "./Home";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <HashRouter>
+      <div className="h-100">
+        <Routes>
+          <Route path="/" element={<Navigate to="Home" />} />
+          <Route path="/Home/*" element={<Home />} />
+          <Route path="/Search/*" element={<Search />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  )
 }
 
 export default App;
