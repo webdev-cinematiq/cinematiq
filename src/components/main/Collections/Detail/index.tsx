0eedb7df97e06336d5bbd3aef64fc1c0acb57c27
Nancy Onyejiaka <nancyonyejiaka@Nancys-MacBook-Pro.local>
2024-07-21 02:23:38 -0400

    fixed collection backdrop styling

--- src/components/main/Collections/Detail/index.tsx
+++ src/components/main/Collections/Detail/index.tsx
@@ -6,8 +6,8 @@ import * as collectionClient from '../../../../services/collectionService';
 import { setCollections } from '../reducer';
 import './index.css';
 
-const CollectionDetail = () => {
-  const { name, titleId } = useParams();
+const CollectionDetail: React.FC = () => {
+  const { name, titleId } = useParams<{ name: string; titleId: string }>();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -20,32 +20,38 @@ const CollectionDetail = () => {
 
   useEffect(() => {
     fetchCollections();
-  }, []);
+  }, [dispatch]);
 
   const collections = collectionsState.collections;
 
-  const collection = collections.find((c: any) => {
-    const user = c.author;
-    return c.titleId === titleId && user.name === name;
-  });
+  const collection =
+    collections.find(
+      (c: any) => c.titleId === titleId && c.author.name === name
+    ) || {};
 
-  if (!collection) {
+  if (!collection || !collection.movies) {
     return <div>Loading...</div>;
   }
 
   return (
     <div className="collection-detail-container">
-      <Image
-        src={collection.movies[0].backdrop}
-        fluid
-        className="collection-backdrop"
-      />
+      {collection.movies[0] && (
+        <div
+          className="collection-backdrop-container"
+          style={{ backgroundImage: `url(${collection.movies[0].backdrop})` }}
+        >
+          <div className="fade-left"></div>
+          <div className="fade-right"></div>
+        </div>
+      )}
+      <div className="horizontal-line"></div>
       <Container>
         <Row className="collection-header">
           <Col>
             <div className="collection-info">
               <h2 className="collection-title">{collection.title}</h2>
               <p className="collection-author">
+                <img src={collection.author.avatar} alt="Author avatar" />
                 collection by {collection.author.name}
               </p>
               <p className="collection-description">{collection.description}</p>
@@ -56,18 +62,15 @@ const CollectionDetail = () => {
           {collection.movies.map((movie: any, index: any) => (
             <Col
               key={movie._id}
-              xs={12}
-              sm={6}
-              md={4}
-              lg={3}
+              xs={6}
+              sm={4}
+              md={3}
+              lg={2}
               className="movie-item"
             >
               <div className="movie-poster-container">
                 <Image src={movie.poster} fluid className="movie-poster" />
                 <div className="movie-index">{index + 1}</div>
-              </div>
-              <div className="movie-title">
-                {movie.title} ({movie.year})
               </div>
             </Col>
           ))}
