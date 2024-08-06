a00fb80b916f0dabc8eb6fd2179fafd9f4472eb7
maikkatko <maikkatko@gmail.com>
2024-08-06 16:39:40 -0400

    update to remove axios

--- src/components/main/Account/Registration/index.tsx
+++ src/components/main/Account/Registration/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import { FaUserCircle } from 'react-icons/fa';
 import * as accountService from "../../../../services/accountService";
